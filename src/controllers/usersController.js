dbdc2e49cbb7da5166e75021f7fc35c2bc230934
mikhita <mikheilispiriani@yahoo.com>
2023-05-09 16:09:15 +0400

    creating the user and asigning the role to it is done

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -4,8 +4,32 @@ const { query } = require('../db');
 
 const getUsers = async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM users');
+    const result = await query('SELECT * FROM users');
     res.status(200).json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+const assignRoleToUser = async (req, res) => {
+  const { user_id, role_name } = req.body;
+
+  try {
+    // Retrieve the role id based on the role name
+    const roleResult = await query(
+      'SELECT id FROM roles WHERE name = $1',
+      [role_name]
+    );
+    const role = roleResult.rows[0];
+
+    // Update the user's role id with the retrieved role id
+    await pool.query(
+      'UPDATE users SET role_id = $1 WHERE id = $2',
+      [role.id, user_id]
+    );
+
+    res.status(200).json({ message: 'Role assigned to user' });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Internal server error' });
@@ -35,7 +59,7 @@ const createUser = async (req, res) => {
 const getUserById = async (req, res) => {
   const { id } = req.params;
   try {
-    const result = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+    const result = await query('SELECT * FROM users WHERE id = $1', [id]);
     if (result.rows.length === 0) {
       res.status(404).json({ message: 'User not found' });
     } else {
@@ -51,7 +75,7 @@ const updateUser = async (req, res) => {
   const { id } = req.params;
   const { name, email, age } = req.body;
   try {
-    const result = await pool.query(
+    const result = await query(
       'UPDATE users SET name = $1, email = $2, age = $3 WHERE id = $4 RETURNING *',
       [name, email, age, id]
     );
@@ -69,7 +93,7 @@ const updateUser = async (req, res) => {
 const deleteUser = async (req, res) => {
   const { id } = req.params;
   try {
-    const result = await pool.query('DELETE FROM users WHERE id = $1 RETURNING *', [id]);
+    const result = await query('DELETE FROM users WHERE id = $1 RETURNING *', [id]);
     if (result.rows.length === 0) {
       res.status(404).json({ message: 'User not found' });
     } else {
@@ -81,4 +105,4 @@ const deleteUser = async (req, res) => {
   }
 };
 
-module.exports = { getUsers, getUserById, createUser, updateUser, deleteUser };
+module.exports = { getUsers, assignRoleToUser, getUserById, createUser, updateUser, deleteUser };
--- src/models/users.js
+++ src/models/users.js
@@ -1,33 +1,34 @@
 // in modules/users.js
 const db = require('../db');
+const { query } = require('../db');
 
 const getAllUsers = async () => {
   const query = 'SELECT * FROM users';
-  const { rows } = await db.query(query);
+  const { rows } = await query(query);
   return rows;
 };
 
 const getUserById = async (userId) => {
   const query = 'SELECT * FROM users WHERE id = $1';
-  const { rows } = await db.query(query, [userId]);
+  const { rows } = await query(query, [userId]);
   return rows[0];
 };
 
 const createUser = async ({ name, email, age, roleId }) => {
   const query = 'INSERT INTO users (name, email, age, role_id) VALUES ($1, $2, $3, $4) RETURNING *';
-  const { rows } = await db.query(query, [name, email, age, roleId]);
+  const { rows } = await query(query, [name, email, age, roleId]);
   return rows[0];
 };
 
 const updateUser = async (userId, { name, email, age, roleId }) => {
   const query = 'UPDATE users SET name = $1, email = $2, age = $3, role_id = $4 WHERE id = $5 RETURNING *';
-  const { rows } = await db.query(query, [name, email, age, roleId, userId]);
+  const { rows } = await query(query, [name, email, age, roleId, userId]);
   return rows[0];
 };
 
 const deleteUser = async (userId) => {
   const query = 'DELETE FROM users WHERE id = $1 RETURNING *';
-  const { rows } = await db.query(query, [userId]);
+  const { rows } = await query(query, [userId]);
   return rows[0];
 };
 
